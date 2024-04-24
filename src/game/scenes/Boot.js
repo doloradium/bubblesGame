fa643f7dd21d6c17b7d85c2d468cd54f039284ee
Valery Makey <valerymakkol@gmail.com>
2024-04-24 19:13:13 +0300

    Added character and background

--- src/game/scenes/Boot.js
+++ src/game/scenes/Boot.js
@@ -1,5 +1,9 @@
 import { Scene } from 'phaser';
 
+let bubble;
+let joyStickX, joyStickY;
+let deltaX, deltaY;
+
 export class Boot extends Scene {
     constructor() {
         super('Boot');
@@ -10,40 +14,57 @@ export class Boot extends Scene {
         this.load.plugin('rexvirtualjoystickplugin', url, true);
         this.load.image('thumb', '../../../public/assets/thumb.png');
         this.load.image('base', '../../../public/assets/base.png');
+        this.load.image('bubble', '../../../public/assets/bubble.png');
+        this.load.image('food', '../../../public/assets/food.png');
+        this.load.image('background', '../../../public/assets/tile.png');
     }
 
     create() {
+
+        const background = this.add.tileSprite(0, 0, 5800, 10000, 'background').setOrigin(0, 0);
+        background.setScale(0.3, 0.3)
+        bubble = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'bubble');
         let base = this.add.image(0, 0, 'base');
         let thumb = this.add.image(0, 0, 'thumb');
         base.displayHeight = window.innerWidth / 3;
         base.displayWidth = window.innerWidth / 3;
         thumb.displayHeight = window.innerWidth / 6;
         thumb.displayWidth = window.innerWidth / 6;
+        joyStickX = window.innerWidth * 0.75;
+        joyStickY = window.innerHeight * 0.85;
         this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
-            x: window.innerWidth * 0.75,
-            y: window.innerHeight * 0.85,
+            x: joyStickX,
+            y: joyStickY,
             radius: 60,
             base: base,
             thumb: thumb,
         });
         this.joystickCursors = this.joyStick.createCursorKeys();
+        console.log(this.joyStick)
     }
 
     update() {
-        // if (this.joystickCursors.up.isDown) {
-        //     jugador.setVelocityY(-200);
-        // }
-        // else if (this.joystickCursors.down.isDown) {
-        //     jugador.setVelocityY(200);
-        // }
-        // else if (this.joystickCursors.left.isDown) {
-        //     jugador.setVelocityX(-200);
-        // }
-        // else if (this.joystickCursors.right.isDown) {
-        //     jugador.setVelocityX(200);
-        // }
-        // else {
-        //     jugador.setVelocity(0);
-        // }
+        // this.background.tilePositionX = this.cameras.main.scrollX;
+        this.cameras.main.centerOn(bubble.x, bubble.y);
+        deltaX = this.joyStick.forceX / 60;
+        deltaY = -this.joyStick.forceY / 60;
+        if (deltaX > 1) {
+            deltaX = 1
+        }
+        if (deltaX < -1) {
+            deltaX = -1
+        }
+        if (deltaY > 1) {
+            deltaY = 1
+        }
+        if (deltaY < -1) {
+            deltaY = -1
+        }
+        console.log('deltaX: ', deltaX, 'deltaY: ', deltaY)
+        bubble.setPosition(bubble.x + deltaX, bubble.y - deltaY);
     }
 }
+
+
+
+
