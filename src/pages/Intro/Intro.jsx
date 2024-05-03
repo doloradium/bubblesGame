cf69e517f1433b24692c7ccbb7e2e5c93498dc14
Valery Makey <valerymakkol@gmail.com>
2024-05-03 23:49:16 +0300

    Fixed intro

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,38 +1,28 @@
 import React, { useEffect } from "react";
 
-import intromp4 from "../../../public/assets/intro.mp4";
 import introwebm from "../../../public/assets/intro.webm";
 
 import styles from "./styles.module.css";
 
 const Intro = () => {
     useEffect(() => {
-        // window.addEventListener("load", (event) => {
-        //     const video = document.querySelector("#video");
-        //     const videoContainer = document.querySelector("#videoContainer");
-        //     video.muted;
-        //     video.pause();
-        //     video.play();
-        // });
-        // setInterval(() => {
-        //     videoContainer.style.display = "none";
-        // }, 8000);
+        const video = document.querySelector("#video");
+        const videoContainer = document.querySelector("#videoContainer");
+        video.addEventListener("ended", (event) => {
+            videoContainer.style.display = "none";
+        });
     }, []);
 
     return (
         <div className={styles.videoContainer} id="videoContainer">
-            {/* <video className={styles.video} controls id="video">
-                <source src={intro} type="video/mp4" />
-            </video> */}
-
             <video
                 controls=""
                 className={styles.video}
                 autoPlay
                 muted
                 name="media"
+                id="video"
             >
-                {/* <source src={intromp4} type="video/mp4" /> */}
                 <source src={introwebm} type="video/webm" />
             </video>
         </div>
